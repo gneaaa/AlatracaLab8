0883f23a7e28ad179ef261e8ae0a73636fc5c874
gneaaa <2015084953@ust-ics.mygbiz.com>
2018-12-21 00:01:23 +0800

    Initial commit

---
+++ app/src/main/java/com/alatraca/nikole/Person.java
@@ -0,0 +1,27 @@
+package com.alatraca.nikole;
+
+public class Person {
+    String name, age, gender;
+
+    public Person() {
+    }
+
+    public Person(String name, String age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
+
